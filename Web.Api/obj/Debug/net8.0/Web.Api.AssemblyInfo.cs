6512be42f1058f1ba833a2da2c3ca1a36a13b5e6
Luis <luisjuniorarias64@gmail.com>
2026-03-09 06:42:29 -0400

    Sln

--- Web.Api/obj/Debug/net8.0/Web.Api.AssemblyInfo.cs
+++ Web.Api/obj/Debug/net8.0/Web.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d76664d0f5a7952a8e4b3b64ec7d29faa5f3fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
